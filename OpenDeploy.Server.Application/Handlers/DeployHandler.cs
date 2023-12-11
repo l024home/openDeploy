4473bd3ab13d830d16c03fbc6f0fe7b1b19f088b
maokuo <352785171@qq.com>
2023-12-11 09:16:27 +0800

    代码优化

--- OpenDeploy.Server.Application/Handlers/DeployHandler.cs
+++ OpenDeploy.Server.Application/Handlers/DeployHandler.cs
@@ -68,26 +68,31 @@ public class DeployHandler(NettyContext context) : AbstractNettyHandler(context)
     /// <summary>
     /// 备份并覆盖旧文件
     /// </summary>
-    private void DoPublish(List<DeployFileInfo> files, string zipFolder, string zipFileName, ProjectConfig projectConfig)
+    private static void DoPublish(List<DeployFileInfo> files, string zipDir, string zipFileName, ProjectConfig projectConfig)
     {
         try
         {
             //先创建备份文件夹
             var backupDir = EnsureBackupDirCreated(zipFileName);
 
+            //遍历每个待发布的文件,依次先备份再替换
             foreach (DeployFileInfo file in files)
             {
-                //源文件路径(解压后的文件)
-                var sourceFileName = Path.Combine(zipFolder, file.PublishFileRelativePath);
+                //文件相对路径(相对于待发布的项目根目录,也是相对于解压后的根目录)
+                var relativeFilePath = file.PublishFileRelativePath;
+
+                //源文件路径(解压后的文件路径)
+                var sourceFileName = Path.Combine(zipDir, relativeFilePath);
 
                 //待发布的文件路径 (服务器真实文件路径)
-                var destFileName = Path.Combine(projectConfig.ProjectDir, file.PublishFileRelativePath);
+                var destFileName = Path.Combine(projectConfig.ProjectDir, relativeFilePath);
 
                 //服务器已存在此文件,先执行备份
                 if (File.Exists(destFileName))
                 {
                     //备份文件路径
-                    var backupFileName = Path.Combine(backupDir, file.PublishFileRelativePath);
+                    var backupFileName = Path.Combine(backupDir, relativeFilePath);
+                    //确保创建备份文件夹
                     var backupFileDir = Path.GetDirectoryName(backupFileName);
                     if (!Directory.Exists(backupFileDir))
                     {
@@ -97,7 +102,7 @@ public class DeployHandler(NettyContext context) : AbstractNettyHandler(context)
                 }
                 else
                 {
-                    //服务器不存在此文件,先创建文件夹层级
+                    //服务器不存在此文件,先创建文件夹层级(比如你新加了一个页面demo.aspx,需要发布到服务器对应的位置)
                     var destFileDir = Path.GetDirectoryName(destFileName);
                     if (!Directory.Exists(destFileDir))
                     {
